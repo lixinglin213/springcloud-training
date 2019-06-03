3d0d69433cbfb3fcb9ae119a09ab388244d4e722
李星霖 <lixinglin@yushl-pc.thunisoft.com>
2019-06-03 21:20:58 +0800

    add springcloud-eureka注册中心

--- springcloud-eureka/src/main/java/com/lixl/springcloud/eureka/SpringcloudEurekaApplication.java
+++ springcloud-eureka/src/main/java/com/lixl/springcloud/eureka/SpringcloudEurekaApplication.java
@@ -1,9 +1,11 @@
-package com.lixl.springcloud.springcloudeureka;
+package com.lixl.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringcloudEurekaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/lixl/springcloud/eureka/SpringcloudTrainingApplication.java
+++ src/main/java/com/lixl/springcloud/eureka/SpringcloudTrainingApplication.java
@@ -1,4 +1,4 @@
-package com.lixl.springcloud.springcloudtraining;
+package com.lixl.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
